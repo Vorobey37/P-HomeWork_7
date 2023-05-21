36a1d09571ec092bdb7a2009131f0999e5296f10
Abvorobey <abvorobey@gmail.com>
2023-05-21 23:29:58 +0300

    Таблица умножения

---
+++ task_36.py
@@ -0,0 +1,36 @@
+"""
+Задача 36: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6), 
+которая принимает в качестве аргумента функцию, вычисляющую элемент по номеру строки и столбца. 
+Аргументы num_rows и num_columns указывают число строк и столбцов таблицы, которые должны быть распечатаны. 
+Нумерация строк и столбцов идет с единицы (подумайте, почему не с нуля). 
+Примечание: бинарной операцией называется любая операция, у которой ровно два аргумента, как, например, 
+у операции умножения.
+
+*Пример:*
+
+**Ввод:** `
+print_operation_table(lambda x, y: x * y)
+
+**Вывод:**
+1 2 3 4 5 6
+2 4 6 8 10 12
+3 6 9 12 15 18
+4 8 12 16 20 24
+5 10 15 20 25 30
+6 12 18 24 30 36
+"""
+
+operation = lambda x, y: x * y
+
+def print_operation_table(operation, num_rows, num_columns):
+    array = [[0]*num_columns for i in range(num_rows)]
+    for i in range(num_rows):
+        for j in range(num_columns):          
+            array[i][j] = operation(i+1, j+1)
+
+    for i in range(len(array)):
+        for j in array[i]:
+            print(j, end = ' ')
+        print()
+
+print_operation_table(operation, 6, 6)
